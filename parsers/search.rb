ae7d64142147d6a235a5b61c2373028a6f07d165
jonathansen0@gmail.com <breageture@gmail.com>
2021-04-02 14:19:18 +0700

    add parser, modify seeder

---
+++ parsers/search.rb
@@ -0,0 +1,31 @@
+# require 'nokogiri'
+# require 'open-uri'
+# require 'json'
+
+# content = URI.open('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=quite+comfort+20&_sacat=0&LH_TitleDesc=0&_odkw=quiet+comfort&_osacat=0').read
+html = Nokogiri.HTML(content)
+
+products = html.css('.srp-results .s-item')
+
+# outputs = []
+
+products.each do |product|
+	name = product.css('.s-item__title').text
+	url = product.css('.s-item__link').attr('href').text
+
+	# content2 = URI.open(url).read
+	# html2 = Nokogiri.HTML(content2)
+	# price = html2.css('#prcIsum').text
+
+	outputs << {
+		# name: name,
+		url: url,
+		# price: price
+		page_type: 'product',
+		vars: {
+			'name' => name
+		}
+	}
+end
+
+# puts JSON.pretty_generate(outputs)
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,18 +1,5 @@
 pages << {
-  page_type: "listings",
-  method: "GET",
-  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  url: "https://www.amazon.com/s/ref=lp_172659_nr_n_0?fst=as%3Aoff&rh=n%3A172282%2Cn%3A%21493964%2Cn%3A1266092011%2Cn%3A172659%2Cn%3A6459737011&bbn=172659&ie=UTF8&qid=1547749731&rnid=172659",
-  vars: {
-    category: "LED & LCD TVs"
-  }
-}
-pages << {
-  page_type: "listings",
-  method: "GET",
-  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  url: "https://www.amazon.com/s/ref=lp_172659_nr_n_1?fst=as%3Aoff&rh=n%3A172282%2Cn%3A%21493964%2Cn%3A1266092011%2Cn%3A172659%2Cn%3A6463520011&bbn=172659&ie=UTF8&qid=1547749731&rnid=172659",
-  vars: {
-    category: "OLED TVs"
-  }
+  url: 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=quite+comfort+20&_sacat=0&LH_TitleDesc=0&_odkw=quiet+comfort&_osacat=0',
+  page_type: 'search'
+
 }
