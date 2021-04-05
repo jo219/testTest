950475443862f714863ce9bbe0feb32f42ee2062
jonathansen0@gmail.com <breageture@gmail.com>
2021-04-05 22:54:32 +0700

    a

--- parsers/product.rb
+++ parsers/product.rb
@@ -3,9 +3,9 @@
 # require 'json'
 
 # content = URI.open('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=quite+comfort+20&_sacat=0&LH_TitleDesc=0&_odkw=quiet+comfort&_osacat=0').read
-puts 'test satu'
+# puts 'test satu'
 html = Nokogiri.HTML(content)
-puts content
+# puts content
 
 # products = html.css('.srp-results .s-item')
 
@@ -18,10 +18,10 @@ puts content
 	# content2 = URI.open(url).read
 	# html2 = Nokogiri.HTML(content2)
 # puts html.css('#prcIsum').text
-puts html.css('[itemprop="price"]').text
+# puts html.css('[itemprop="price"]').text
 
 price = html.css('[itemprop="price"]').text
-raise 'something 2 happen!' + price if price.strip == ''
+raise 'something happen!' + price if price.strip == ''
 
 outputs << {
 	_collection: "products",
